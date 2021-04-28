ebbf922b17267d7290bec1e006966f120478532d
znex07 <znex07@gmail.com>
2021-04-28 15:52:16 +0800

    updateagain

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                  Welcome to ActivePass!!
+                  Welcome to ActivePass!!!!!!!!!!!!
                 </div>
 
                 <div class="links">
